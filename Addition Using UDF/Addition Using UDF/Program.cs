2eadac4db02a38710dbcc61c5af67cc488911043
prashantpatel-807 <122272169+prashantpatel-807@users.noreply.github.com>
2024-03-03 12:47:44 +0530

    Addition Using UDF
    Add - Addition Using User Defined Function (UDF)

---
+++ Addition Using UDF/Addition Using UDF/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Addition_Using_UDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Enter No1:- ");
+            int i = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Enter No2:- ");
+            int j = int.Parse(System.Console.ReadLine());
+
+            int k = sum(i, j);
+            System.Console.WriteLine("Sum Of Two Numbers are:- " + k);
+
+            k = Sub(i,j);
+            System.Console.WriteLine("Substraction Of Two Numbers are:- " + k);
+
+            k = mul(i,j);
+            System.Console.WriteLine("Muliplication Of Two Numbers are:- " + k);
+
+            k = div(i,j);
+            System.Console.WriteLine("Divison Of Two Numbers are:- " + k);
+            System.Console.ReadLine();
+        }
+
+        static int sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        static int div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Addition Using UDF/Addition Using UDF/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Addition Using UDF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("94262-51074")]
+[assembly: AssemblyProduct("Addition Using UDF")]
+[assembly: AssemblyCopyright("Copyright © 94262-51074 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7d5ee87f-d20a-4329-b5b2-a37daab3c938")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
